47e706b7c2782b500dd25e8a3a7b17448d249cb3
Ujwal Khatiwada <134910305+ujwal-khatiwada@users.noreply.github.com>
2025-07-22 10:05:25 +0545

    Updated submit button

--- app/component/contactdetail.tsx
+++ app/component/contactdetail.tsx
@@ -107,7 +107,7 @@ const ContactDetail = () => {
           <div className="w-full flex justify-end">
             <button
               className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-full bg-blue-600 text-white shadow-md shadow-blue-900/50 hover:shadow-[5px_5px_20px_5px_rgb(37,99,235)] focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none w-full md:w-fit"
-              type="button"
+              type="submit"
             >
               Send message
             </button>
