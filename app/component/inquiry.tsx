55fa62b6777797501a190067ba3724db2ae40b5b
Ujwal Khatiwada <134910305+ujwal-khatiwada@users.noreply.github.com>
2025-07-23 17:07:39 +0545

    Create inquiry.tsx

---
+++ app/component/inquiry.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const inquiry = () => {
+  return (
+    <a href="tel:+9779764468567">
+      <button 
+      className="inline-block bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition"
+    >
+        Contact here for inquiry
+      </button>
+    </a>
+  )
+}
+
+export default inquiry
