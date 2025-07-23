bce463b30b9fecd1a1c7a4e767d061338d79896f
Ujwal Khatiwada <134910305+ujwal-khatiwada@users.noreply.github.com>
2025-07-23 16:59:18 +0545

    Created callnow.tsx

---
+++ app/component/callnow.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function callnow() {
+  return (
+    <a href="tel:+9779764468567">
+      <button className="mt-6 px-6 py-2 rounded-full font-bold text-white bg-blue-600 hover:shadow-[5px_5px_20px_5px_rgb(37,99,235)] transition-all">
+        Call Now
+      </button>
+    </a>
+  )
+}
+
+export default callnow
